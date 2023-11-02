a2b5b24202b69b1e736c445556b5442d38711292
hodin030 <hodin030@gmail.com>
2023-11-02 16:10:33 +0900

    first commit

---
+++ src/main/java/com/singsongchanson/SingsongchansonApplication.java
@@ -0,0 +1,13 @@
+package com.singsongchanson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingsongchansonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingsongchansonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/singsongchanson/SingsongchansonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singsongchanson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingsongchansonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
