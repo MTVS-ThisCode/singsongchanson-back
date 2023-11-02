92a139035ba25bafa40161f7e545a6e2fb2f26a6
hodin030 <hodin030@gmail.com>
2023-11-02 17:39:46 +0900

    feat: AppProperties 클래스 생성

--- src/main/java/com/singsongchanson/SingsongchansonApplication.java
+++ src/main/java/com/singsongchanson/SingsongchansonApplication.java
@@ -1,9 +1,12 @@
 package com.singsongchanson;
 
+import com.singsongchanson.global.configuration.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class SingsongchansonApplication {
 
     public static void main(String[] args) {
