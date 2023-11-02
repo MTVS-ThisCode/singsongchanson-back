d2370eac57b69de78bf75b5d6ecb5c85a6dc99bc
hodin030 <hodin030@gmail.com>
2023-11-02 17:40:37 +0900

    feat: User 정보 요청 기능 추가

---
+++ src/main/java/com/singsongchanson/global/security/jwt/command/infrastructure/service/RequestUserInfraService.java
@@ -0,0 +1,20 @@
+package com.singsongchanson.global.security.jwt.command.infrastructure.service;
+
+import com.singsongchanson.domain.user.command.application.dto.FindUserResponseDTO;
+import com.singsongchanson.domain.user.query.application.service.UserQueryService;
+import com.singsongchanson.global.security.jwt.domain.service.RequestUserDomainService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RequestUserInfraService implements RequestUserDomainService {
+
+    private final UserQueryService userQueryService;
+
+    @Override
+    public FindUserResponseDTO getUserByUserNo(Long userNo) {
+        return userQueryService.findUserByUserNo(userNo);
+    }
+
+}
