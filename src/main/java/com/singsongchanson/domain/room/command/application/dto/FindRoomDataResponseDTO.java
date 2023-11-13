6d631742a4183e7287052760bbc560a8f34e9d7f
hodin030 <hodin030@gmail.com>
2023-11-13 23:57:50 +0900

    feat: 조회된 RoomData 응답 DTO 생성

---
+++ src/main/java/com/singsongchanson/domain/room/command/application/dto/FindRoomDataResponseDTO.java
@@ -0,0 +1,19 @@
+package com.singsongchanson.domain.room.command.application.dto;
+
+import com.singsongchanson.domain.room.command.domain.aggregate.vo.Furniture;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+public class FindRoomDataResponseDTO {
+
+    private String roomId;
+    private List<Furniture> furniture;
+    private String userName;
+    private String userProfileImg;
+}
