6e110ad52a255e434d65c499290787412db2a662
hodin030 <hodin030@gmail.com>
2023-11-13 23:58:46 +0900

    refactor: roomId로 RoomData 조회 시 유저 정보 포함해서 리턴하도록 수정

--- src/main/java/com/singsongchanson/domain/room/query/application/service/RoomQueryService.java
+++ src/main/java/com/singsongchanson/domain/room/query/application/service/RoomQueryService.java
@@ -1,8 +1,10 @@
 package com.singsongchanson.domain.room.query.application.service;
 
+import com.singsongchanson.domain.room.command.application.dto.FindRoomDataResponseDTO;
 import com.singsongchanson.domain.room.command.application.dto.RoomDataResponseDTO;
 import com.singsongchanson.domain.room.command.application.dto.RoomResponseDTO;
 import com.singsongchanson.domain.room.command.domain.aggregate.entity.Room;
+import com.singsongchanson.domain.room.command.domain.aggregate.entity.RoomData;
 import com.singsongchanson.domain.room.command.domain.repository.RoomDataRepository;
 import com.singsongchanson.domain.room.command.domain.repository.RoomRepository;
 import com.singsongchanson.domain.room.query.domain.service.RoomQueryDomainService;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,27 +30,40 @@ public class RoomQueryService {
 
         List<RoomResponseDTO> roomResponseList = new ArrayList<>();
 
-        for(Room room : roomList) {
-           Long userNo = room.getRoomOwnerVO().getUserNo();
-           FindUserResponseDTO findUserResponse = roomQueryDomainService.getUserInfo(userNo);
+        for (Room room : roomList) {
+            Long userNo = room.getRoomOwnerVO().getUserNo();
+            FindUserResponseDTO findUserResponse = roomQueryDomainService.getUserInfo(userNo);
 
-           RoomResponseDTO roomResponseDTO = new RoomResponseDTO(
-                   room.getRoomId(),
-                   userNo,
-                   findUserResponse.getNickName(),
-                   findUserResponse.getProfileImg()
-           );
-           roomResponseList.add(roomResponseDTO);
+            RoomResponseDTO roomResponseDTO = new RoomResponseDTO(
+                    room.getRoomId(),
+                    userNo,
+                    findUserResponse.getNickName(),
+                    findUserResponse.getProfileImg()
+            );
+            roomResponseList.add(roomResponseDTO);
         }
         return roomResponseList;
     }
 
-    public RoomDataResponseDTO findRoomDataById(String roomId) {
+    public FindRoomDataResponseDTO findRoomDataById(String roomId) {
 
-        RoomDataResponseDTO roomDataResponse = roomDataRepository.findById(roomId)
-                .map(RoomDataResponseDTO::from)
-                .orElseThrow(() -> new IllegalArgumentException("해당하는 데이터가 없습니다."));
+        Optional<Room> room = roomRepository.findById(roomId);
+        Optional<RoomData> roomData = roomDataRepository.findById(roomId);
 
-        return roomDataResponse;
+        if (room.isPresent() && roomData.isPresent()) {
+            Long userNo = room.get().getRoomOwnerVO().getUserNo();
+            RoomData findedRoomData = roomData.get();
+            FindUserResponseDTO findUserResponse = roomQueryDomainService.getUserInfo(userNo);
+
+            FindRoomDataResponseDTO findRoomDataResponse = new FindRoomDataResponseDTO(
+                    findedRoomData.getId(),
+                    findedRoomData.getFurniture(),
+                    findUserResponse.getNickName(),
+                    findUserResponse.getProfileImg());
+
+            return findRoomDataResponse;
+        }
+
+        return null;
     }
 }
