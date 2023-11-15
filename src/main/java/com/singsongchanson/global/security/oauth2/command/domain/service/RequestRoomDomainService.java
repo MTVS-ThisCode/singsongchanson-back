68abdf8551d5b6844073b17eaed837055481edad
hodin030 <hodin030@gmail.com>
2023-11-15 16:55:19 +0900

    feat: 룸 생성기능 요청하는 RequestRoomDomainService 생성

---
+++ src/main/java/com/singsongchanson/global/security/oauth2/command/domain/service/RequestRoomDomainService.java
@@ -0,0 +1,9 @@
+package com.singsongchanson.global.security.oauth2.command.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RequestRoomDomainService {
+
+    void createRoomService(Long userNo);
+}
