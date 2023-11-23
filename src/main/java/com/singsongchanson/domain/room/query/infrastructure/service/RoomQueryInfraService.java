89d727a7fc7f27cf24f51e1279af0a6634de8b26
hodin030 <hodin030@gmail.com>
2023-11-23 10:23:15 +0900

    feat: roomId로 댓글 조회 인프라 서비스

--- src/main/java/com/singsongchanson/domain/room/query/infrastructure/service/RoomQueryInfraService.java
+++ src/main/java/com/singsongchanson/domain/room/query/infrastructure/service/RoomQueryInfraService.java
@@ -1,21 +1,35 @@
 package com.singsongchanson.domain.room.query.infrastructure.service;
 
+import com.singsongchanson.domain.comment.command.application.dto.CommentResponseDTO;
+import com.singsongchanson.domain.comment.query.service.CommentQueryService;
 import com.singsongchanson.domain.room.query.domain.service.RoomQueryDomainService;
 import com.singsongchanson.domain.user.command.application.dto.FindUserResponseDTO;
 import com.singsongchanson.domain.user.query.application.service.UserQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class RoomQueryInfraService implements RoomQueryDomainService {
 
     private final UserQueryService userQueryService;
+    private final CommentQueryService commentQueryService;
+
     @Override
     public FindUserResponseDTO getUserInfo(Long userNo) {
 
         FindUserResponseDTO findUserResponse = userQueryService.findUserByUserNo(userNo);
 
         return findUserResponse;
+    }
+
+    @Override
+    public List<CommentResponseDTO> getRoomComment(String roomId) {
+
+        List<CommentResponseDTO> commentResponse = commentQueryService.findByRoomId(roomId);
+
+        return commentResponse;
     }
 }
